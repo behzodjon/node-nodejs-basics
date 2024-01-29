d189f508128d0d008661cbc0440f7817a8e26114
behzodjon <behzodjontuit1998@gmail.com>
2024-01-29 19:17:44 +0500

    feat: add solutions

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-    // Write your code here 
+    const prefix = 'RSS_';
+    const envVars = Object.keys(process.env)
+        .filter(key => key.startsWith(prefix))
+        .map(key => `${key.slice(prefix.length)}=${process.env[key]}`);
+
+    console.log(envVars.join('; '));
 };
 
 parseEnv();
