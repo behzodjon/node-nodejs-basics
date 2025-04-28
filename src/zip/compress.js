93874e94294e227770a780f09cfaa5de0ca11e1c
Behzod Xudoyberdiev <behzodjontuit1998@gmail.com>
2025-04-28 16:29:46 +0500

    feat: add solution zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,22 @@
+import fs from 'fs';
+import zlib from 'zlib';
+import path from 'path';
+
+const __dirname = path.resolve();
+
 const compress = async () => {
-    // Write your code here 
+    const fileToCompress = path.join(__dirname, 'files', 'fileToCompress.txt');
+    const archive = path.join(__dirname, 'files', 'archive.gz');
+
+    if (!fs.existsSync(fileToCompress) || fs.existsSync(archive)){
+        throw new Error('FS operation failed');
+    }
+
+    const readFile = fs.createReadStream(fileToCompress);
+    const writeIntoArchive = fs.createWriteStream(archive);
+    const compressedZip = zlib.createGzip();
+
+    readFile.pipe(compressedZip).pipe(writeIntoArchive);
 };
 
 await compress();
